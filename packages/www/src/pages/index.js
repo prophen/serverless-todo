20ed4fccb7d45761a8c17953fd844a8685a08bf9
Nikema Prophet <nikema.prophet@popschools.com>
2020-02-15 14:10:01 -0800

    Netlify Identity

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={() => {
-          alert("clicked");
-        }}
-      >
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-);
+import netlifyIdentity from "netlify-identity-widget";
+
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            console.log(netlifyIdentity.currentUser());
+          }}
+        >
+          Log user
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
