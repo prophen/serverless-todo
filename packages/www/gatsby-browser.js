628270858269341e4d47ed1d624325f4292dfd03
Nikema Prophet <nikema.prophet@popschools.com>
2020-02-15 23:26:34 -0800

    Access user in graphql lambda context

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,3 +1,39 @@
-const React = require("react")
-const wrapRootElement = require("./wrap-root-element.js")
-exports.wrapRootElement = wrapRootElement
+const React = require("react");
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache
+} = require("@apollo/client");
+const { setContext } = require("apollo-link-context");
+const netlifyIdentity = require("netlify-identity-widget");
+
+const wrapRootElement = require("./wrap-root-element");
+
+const authLink = setContext((_, { headers }) => {
+  const user = netlifyIdentity.currentUser();
+  const token = user.token.access_token;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: "https://serverless-todo-prophen.netlify.com/.netlify/functions/graphql"
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
+});
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
